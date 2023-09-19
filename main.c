ff69ef571a67457ce8f42d2bbe1404d1b132d12a
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-09-19 01:35:38 -0600

    Stubbed out main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+// Standard library
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include
+#include <set/set.h>
+
+// Entry point
+int main ( int argc, const char* argv[] )
+{
+
+    // Initialized data
+    set *p_set = (void *) 0;
+
+    // Success
+    return EXIT_SUCCESS;
+}
